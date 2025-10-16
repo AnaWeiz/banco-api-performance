61e3c0a12d46e807d51bd488eafb6d4f76209baa
AnaWeiz <ana_laura_w@hotmail.com>
2025-10-15 23:20:01 -0300

    Adicionei fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,13 +1,14 @@
 import http from 'k6/http'; //faz requisições
 import { sleep, check } from 'k6'; //função para esperar por alguns segundos
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = { //criar constante já exportando
-    stages: [
+  stages: [
         { duration: '5s', target: 10 }, //durante Xs, coloque Y usuários virtuais para executar meu teste
         { duration: '20s', target: 10 }, //nos próximos Xs, Y usuários virtuais
         { duration: '5s', target: 0 }
     ],
-    thresholds: { //falha o teste se não for validado
+  thresholds: { //falha o teste se não for validado
         http_req_duration: ['p(90)<3000', 'max<5000'], //duração da requisão - percentil de noventa (90%) deve ter tempo de duração < 3000 e a requisição com maior tempo deve ser < 5000  
         http_req_failed: ['rate<0.01'] //quantidade de requisições falhas (erros)
     } 
@@ -16,10 +17,9 @@ export const options = { //criar constante já exportando
 export default function () { //exporta teste (tem 1 por arquivo)
   const url = 'http://localhost:3000/login';
   
-  const payload = JSON.stringify({ //request body
-    username: 'julio.lima',
-    senha: '123456',
-  });
+  postLogin.username = "junior.lima"
+  console.log(postLogin)
+  const payload = JSON.stringify(postLogin);
 
   const params = {
     headers: {
